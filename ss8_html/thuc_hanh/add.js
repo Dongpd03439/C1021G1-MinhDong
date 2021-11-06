a72a6ae39bb9647c2edc90ae199dd46e271e068c
unknown <you@example.com>
2021-11-06 10:09:56 +0700

    thuc_hanh

---
+++ ss8_html/thuc_hanh/add.js
@@ -0,0 +1,8 @@
+let a = prompt('nhap gia tri :');
+let b = prompt('nhap gia tri :');
+let result = (a + b)<4;
+if(result < 4){
+    alert('Over');
+}else{
+    alert('below');
+}
---
+++ ss8_html/thuc_hanh/coding.js
@@ -0,0 +1,12 @@
+let a = prompt("Type a number", 0);
+switch (a) {
+    case a <0:
+    alert(1);
+    break;
+    case a > 0:
+    alert(-1);
+    break;
+    case a = 0:
+     alert(0);
+     break;
+}
