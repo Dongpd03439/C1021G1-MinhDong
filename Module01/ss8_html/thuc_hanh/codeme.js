0dc4a9ecf54bd38b706041dd65b2cd5a0310ebdc
unknown <you@example.com>
2021-12-01 17:56:35 +0700

    module2

--- Module01/ss8_html/thuc_hanh/codeme.js
+++
@@ -1,6 +0,0 @@
-let value = prompt("What is the “official” name of JavaScript?");
-if(value == ECMAScript){
-    alert('Right!');
-}else{
-    alert("you don't know? ECMAScrtp");
-}
--- README.md.md
+++
@@ -1,1 +0,0 @@
-# C1021G1-MinhDong
